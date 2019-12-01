7bcfd292375b190bc3a8aaa2fde6eb324a0b2621
Tim Humphries <tim@utf8.me>
2019-12-01 15:54:05 -0800

    Silence autorandr in logs

--- xinitrc/default.nix
+++ xinitrc/default.nix
@@ -6,10 +6,10 @@ let
     ${xsetroot}/bin/xsetroot -cursor_name left_ptr
     ${setxkbmap}/bin/setxkbmap -option ctrl:nocaps
     ${xset}/bin/xset r rate 180 25
-    ${autorandr}/bin/autorandr --change --skip-options=gamma --default clone-largest
+    ${autorandr}/bin/autorandr --change --skip-options=gamma --default clone-largest &>/dev/null
     while true; do
       sleep 5
-      ${autorandr}/bin/autorandr --change --skip-options=gamma --default clone-largest
+      ${autorandr}/bin/autorandr --change --skip-options=gamma --default clone-largest &>/dev/null
     done &
     ${redshift}/bin/redshift &
     ${xsettingsd}/bin/xsettingsd &
