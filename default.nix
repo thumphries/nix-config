855ca71dc9a0b8d7b65b984ea3484625a7fbc4de
Tim Humphries <tim@utf8.me>
2019-12-09 23:28:50 -0800

    Lazily compile go tools

--- default.nix
+++ default.nix
@@ -256,6 +256,25 @@ let
     xalt = xalt;
     xsettingsd = xsettingsd;
   };
+
+  gocmds = nixpkgs.pkgs.callPackage ./gocmds {
+    goinstalls = [
+      {
+        name = "ghq";
+        package = "github.com/motemen/ghq";
+        revision = "7ba9b5f82952dc930f289262a2df7bafb6bd53bf";
+        sha = "2b4a72eb1f387f9a6246d830105d8156bf1fbc26";
+        binaries = ["ghq"];
+      }
+      {
+        name = "wtf";
+        package = "github.com/wtfutil/wtf";
+        revision = "5291a31afd9a525342ab62896423a00e06f3811f";
+        sha = "49de074784f3b08a764bbe453db893e7d7dfe78a";
+        binaries = ["wtf"];
+      }
+    ];
+  };
 in
   nixpkgs.pkgs.buildEnv rec {
     name = "nix-config";
@@ -282,6 +301,9 @@ in
 
       # haskell
       nixpkgs.pkgs.ghc
+
+      # go tools
+      gocmds
 
       # git et al
       nixpkgs.pkgs.cacert
---
+++ gocmds/default.nix
@@ -0,0 +1,115 @@
+{ stdenv, symlinkJoin, writeTextFile, go, musl, goinstalls }:
+let
+  goLazyBuild = writeTextFile {
+      name = "go-lazy-build";
+      executable = true;
+      destination = "/bin/go-lazy-build";
+      text = ''
+        #!/bin/sh -eu
+        PKG=$1
+        REV=$2
+        SHA=$3
+        OUT=$4
+
+        >&2 echo "Building $PKG..."
+
+        TMPBINDIR=$(mktemp -d)
+        cd $TMPBINDIR
+        git clone "https://$PKG" src
+        cd src
+        git checkout "$REV"
+        git submodule init
+        git submodule sync
+        git submodule update
+        rm -rf .git
+
+        hash_it () {
+          find . ! -path . -type f \( -exec sha1sum {} \; \) 2>&1 \
+            | awk '{print $1}' | sort \
+            | sha1sum | awk '{print $1}'
+        }
+
+        check_it () {
+          GOT=$(hash_it)
+          if [ ! "$GOT" = "$SHA" ]; then
+            echo "EXPECT: $SHA"
+            echo "ACTUAL: $GOT"
+            echo "Source tree did not hash properly"
+            exit 1
+          fi
+        }
+
+        check_it
+        ${go}/bin/go mod download
+        ${go}/bin/go mod verify
+
+        export GOBIN="''${TMPBINDIR}/bin"
+        mkdir -p $GOBIN
+        unset GOPATH
+        export CC=${musl.dev}/bin/musl-gcc
+        ${go}/bin/go install --ldflags "-linkmode external -extldflags \"-static\"" ./...
+
+        mkdir -p "$OUT"
+        find "$GOBIN" -type f -executable -exec cp {} $OUT/ \;
+        find "$OUT" -type f -executable
+
+        rm -rf "''${TMPBINDIR}"
+      '';
+    };
+
+  goLazy = writeTextFile {
+      name = "go-lazy";
+      executable = true;
+      destination = "/bin/go-lazy";
+      text = ''
+        #!/bin/sh -eu
+
+        PKG="$1"
+        REV="$2"
+        SHA="$3"
+
+        OUTDIR="$HOME/.cmd/$PKG/$REV"
+
+        if [ ! -d "$OUTDIR" ]; then
+          ${goLazyBuild}/bin/go-lazy-build "$PKG" "$REV" "$SHA" "$OUTDIR"
+        fi
+        echo ''${OUTDIR}
+      '';
+    };
+
+    builder = pkg :
+      ''${goLazy}/bin/go-lazy "${pkg.package}" "${pkg.revision}" "${pkg.sha}"'';
+    buildAll =
+      builtins.map (pkg: builder pkg) goinstalls;
+
+    goStrict = writeTextFile {
+      name = "go-strict";
+      executable = true;
+      destination = "/bin/go-strict";
+      text = ''
+        #!/bin/sh -eu
+        ${builtins.concatStringsSep "\n" buildAll}
+      '';
+    };
+
+    goBins =
+      builtins.map
+        (pkg :
+          builtins.map
+            (bin :
+              writeTextFile {
+                name = pkg.name + "-" + bin;
+                executable = true;
+                destination = "/bin/" + bin;
+                text = ''
+                  #!/bin/sh -eu
+                  OUT=$(${builder pkg})
+                  exec "$OUT/${bin}" "$@"
+                '';
+              }) pkg.binaries) goinstalls;
+in
+  symlinkJoin {
+      name = "gocmds";
+      paths = [ (builtins.concatLists goBins) goStrict ];
+      buildInputs = [ go musl.dev goLazy goLazyBuild ];
+    }
